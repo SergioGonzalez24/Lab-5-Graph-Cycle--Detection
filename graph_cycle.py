31f32d8cccd0ff0dc4ab12626e9d0cde4bfc40a1
sergiogonzalez24 <sergiomanuel2020@gmail.com>
2022-09-30 09:27:03 -0500

    Creacion de archivos

---
+++ graph_cycle.py
@@ -0,0 +1,19 @@
+
+# ----------------------------------------------------------
+# Lab #5: Graph Cycle Detection
+#
+# Date: 07-Oct-2022
+# Authors:
+#           A01745446 Sergio Manuel Gonzalez Vargas
+#           A01720627 Rodrigo Alfredo Mendoza España
+# ----------------------------------------------------------
+
+
+from typing import Optional
+
+Graph = dict[str, list[str]]
+
+
+def has_cycle(initial: str, graph: Graph) -> Optional[list[str]]:
+    # The function's code goes here
+    ...
